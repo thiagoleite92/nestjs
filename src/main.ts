0a2ece5dbbd9f3a6a9ee97066fd29cdf686a0279
Thiago Leite <leite.tjs@gmail.com>
2023-06-13 20:28:04 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
     origin: ['http://localhost:4200'],
