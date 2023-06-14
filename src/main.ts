dbcb2d07b1ae15168c0af6396483267895fa212c
Thiago Leite <leite.tjs@gmail.com>
2023-06-14 00:59:00 -0300

    fix cors again

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
-    origin: '*',
+    origin: ['http://localhost:4200/'],
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
